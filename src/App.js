d2921bd2d3042e153e15442890840c3bf1db2085
Ketki Thakre <ketkithakre@ketkis-mbp-3.myfiosgateway.com>
2023-06-02 23:41:50 -0400

    design lib and initial screens

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import Button from 'react-bootstrap/Button';
+
+import CustomTable from './components/CustomTable';
 
 function App() {
   return (
     <div className="App">
+      <div className="transaction_list">
+        <div className="paying">
+          <p>Paying</p>
+          <CustomTable />
+        </div>
+        <div className="receiving">
+          <p>Receiving</p>
+          <CustomTable />
+        </div>
+      </div>
 
+      <div className="transaction_action">
+        <Button variant="primary">Add new Transaction</Button>
+        <Button variant="primary">Compress Transactions</Button>
+      </div>
     </div>
   );
 }
